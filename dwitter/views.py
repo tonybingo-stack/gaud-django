fbff750127348363633c81d7c940af872977fae6
tonybingo-stack <tonybingo19970425@outlook.com>
2022-11-14 12:41:24 -0800

    update ui

--- dwitter/views.py
+++ dwitter/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from .models import Profile
 from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.csrf import requires_csrf_token
 
 def dashboard(request):
     return render(request, "dwitter/dashboard.html")
@@ -11,7 +10,6 @@ def profile_list(request):
     profiles = Profile.objects.exclude(user=request.user)
     return render(request, "dwitter/profile_list.html", {"profiles": profiles})
 
-@requires_csrf_token
 def profile(request, pk):
     profile = Profile.objects.get(pk=pk)
     if request.method == "POST":
