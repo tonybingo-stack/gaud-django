c42d0fe232941f5d58731f8fcecaebad535f8fce
tonybingo-stack <tonybingo19970425@outlook.com>
2022-11-14 10:47:01 -0800

    fix application error csrf-5

--- dwitter/views.py
+++ dwitter/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from .models import Profile
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import requires_csrf_token
 
-@csrf_exempt
 def dashboard(request):
     return render(request, "dwitter/dashboard.html")
 
@@ -11,7 +11,7 @@ def profile_list(request):
     profiles = Profile.objects.exclude(user=request.user)
     return render(request, "dwitter/profile_list.html", {"profiles": profiles})
 
-@csrf_exempt
+@requires_csrf_token
 def profile(request, pk):
     profile = Profile.objects.get(pk=pk)
     if request.method == "POST":
